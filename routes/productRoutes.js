efa376990e5ca81bb9e6da366712d0913231cf8a
bduytuan244 <bdtuan244@gmail.com>
2026-02-01 18:52:22 +0700

    Add File Upload

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,19 +6,27 @@ const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 const { check } = require('express-validator');
 const { checkValidation } = require('../middleware/validate');
 
+const upload = require('../config/cloudinary');
+
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 
+
 router.post('/', 
   verifyToken, 
-  verifyAdmin, 
+  verifyAdmin,
+  
+  upload.array('images', 5), 
+
   [
     check('name', 'Tên sản phẩm là bắt buộc').not().isEmpty(),
     check('price', 'Giá phải là số và lớn hơn 0').isFloat({ gt: 0 }),
     check('category', 'Danh mục là bắt buộc').not().isEmpty(),
     check('brand', 'Thương hiệu là bắt buộc').not().isEmpty()
   ],
+  
   checkValidation, 
+  
   productController.createProduct
 );
 
