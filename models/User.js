3becccd9313a38d658e165bf0797044aa45a3124
bduytuan244 <bdtuan244@gmail.com>
2026-02-07 10:48:29 +0700

    Fix register

--- models/User.js
+++ models/User.js
@@ -1,18 +1,47 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); 
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true }, // uniqe email
-  password: { type: String, required: true }, // Mã hóa password
-  phone: { type: String },
-  address: { type: String },
-  isAdmin: { type: Boolean, default: false }, 
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  refreshToken: { type: String },
+  name: {
+    type: String,
+    required: [true, "Vui lòng nhập tên"],
+  },
+  email: {
+    type: String,
+    required: [true, "Vui lòng nhập email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Vui lòng nhập đúng định dạng email",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Vui lòng nhập mật khẩu"],
+    minlength: 6,
+    select: false, 
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
   isVerified: { type: Boolean, default: false },
-  otp: { type: String }, 
+  otp: { type: String },
   otpExpire: { type: Date },
+  refreshToken: { type: String }
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
