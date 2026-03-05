36c290e36fb81beeeb3cda25a54dd11cbe5cd71c
bduytuan244 <bdtuan244@gmail.com>
2026-03-05 20:28:46 +0700

    Update productroutes for seller

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,7 +7,7 @@ const upload = require('../config/cloudinary');
 
 const validate = require('../middleware/validateMiddleware');
 const { createProductSchema, updateProductSchema } = require('../validations/productValidation');
-
+router.get('/myshop', verifyToken, verifySeller, productController.getSellerProducts);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 
