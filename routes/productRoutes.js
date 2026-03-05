c5e421ce81459c01437436bfe65ae47a2577d016
bduytuan244 <bdtuan244@gmail.com>
2026-03-05 20:36:34 +0700

    Fix update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
+const { verifyToken, verifyAdmin, verifySeller } = require('../middleware/authMiddleware');
 
 const upload = require('../config/cloudinary');
-
 const validate = require('../middleware/validateMiddleware');
 const { createProductSchema, updateProductSchema } = require('../validations/productValidation');
+
 router.get('/myshop', verifyToken, verifySeller, productController.getSellerProducts);
 router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 
 router.post('/', 
   verifyToken, 
-  verifyAdmin,
+  verifySeller,
   upload.array('images', 5),
   validate(createProductSchema), 
   productController.createProduct
@@ -21,7 +21,7 @@ router.post('/',
 
 router.put('/:id', 
   verifyToken, 
-  verifyAdmin, 
+  verifySeller, 
   upload.array('images', 5), 
   validate(updateProductSchema),
   productController.updateProduct
@@ -29,7 +29,7 @@ router.put('/:id',
 
 router.delete('/:id', 
   verifyToken, 
-  verifyAdmin, 
+  verifySeller, 
   productController.deleteProduct
 );
 
