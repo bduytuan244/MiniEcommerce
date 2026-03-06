722cadd0373c8a115347ace08722237163284427
bduytuan244 <bdtuan244@gmail.com>
2026-03-06 20:24:11 +0700

    Fix get order user, fixseller page button

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-
 const { verifyToken, verifyAdmin, verifySeller } = require('../middleware/authMiddleware');
 
 router.get('/myshop', verifyToken, verifySeller, orderController.getSellerOrders);
 
-router.post('/', verifyToken, orderController.createOrder);
+router.get('/myorders', verifyToken, orderController.getMyOrders); 
 
-router.get('/:id', verifyToken, orderController.getOrderById);
+router.post('/', verifyToken, orderController.createOrder);
 router.get('/', verifyToken, verifyAdmin, orderController.getOrders);
 
+router.get('/:id', verifyToken, orderController.getOrderById);
 router.put('/:id/status', verifyToken, verifyAdmin, orderController.updateOrderStatus);
-
 router.put('/:id/pay', verifyToken, orderController.updateOrderToPaid);
 
 module.exports = router;
