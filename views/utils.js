7f0bd6744799711f1b3b9724540777ff163056a7
bduytuan244 <bdtuan244@gmail.com>
2026-03-20 10:32:06 +0700

    update button

--- views/utils.js
+++ views/utils.js
@@ -13,23 +13,39 @@ function getAuthHeaders() {
 function checkLoginState() {
     const token = localStorage.getItem('token');
     const userStr = localStorage.getItem('user');
-
+    
     const loginLink = document.getElementById('login-link');
     const userMenu = document.getElementById('user-menu');
     const userNameDisplay = document.getElementById('user-name-display');
+    const sellerLink = document.getElementById('link-seller-center'); 
 
-    if (token && userStr && loginLink && userMenu) {
+    if (token && userStr) {
         const user = JSON.parse(userStr);
         
-        loginLink.style.display = 'none'; 
-        userMenu.style.display = 'flex';  
-        
-        if (userNameDisplay) {
-            userNameDisplay.innerText = user.name; 
-        }
-    }
+        if (loginLink) loginLink.style.display = 'none';
+        if (userMenu) userMenu.style.display = 'flex';
+        if (userNameDisplay) userNameDisplay.innerText = user.name;
 
-    updateCartBadge();
+        if (user.isSeller === true) {
+            if (sellerLink) {
+                sellerLink.style.display = 'flex';
+            }
+        } else {
+            if (sellerLink) {
+                sellerLink.style.display = 'none';
+            }
+        }
+    } else {
+        if (loginLink) loginLink.style.display = 'flex';
+        if (userMenu) userMenu.style.display = 'none';
+        if (sellerLink) sellerLink.style.display = 'none';
+    }
+}
+
+function logoutUser() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    window.location.href = '../home/index.html'; 
 }
 
 function updateCartBadge() {
