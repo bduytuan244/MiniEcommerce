bbe7233f7667c6b33954934284c0b0216df62903
bduytuan244 <bdtuan244@gmail.com>
2026-03-25 17:48:48 +0700

    Update login page

--- MiniEcommerce-Frontend/src/App.jsx
+++ MiniEcommerce-Frontend/src/App.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Header from './components/Header'; // Nhúng Header vào đây
+import Header from './components/Header';
+import Login from './pages/Login';
 
-const Login = () => <div style={{padding: '20px'}}><h1>Đăng Nhập</h1></div>;
 const Cart = () => <div style={{padding: '20px'}}><h1>Giỏ Hàng</h1></div>;
 const ProductDetail = () => <div style={{padding: '20px'}}><h1>Chi Tiết Sản Phẩm</h1></div>;
 
 function App() {
   return (
     <Router>
-      {/* Header đặt bên ngoài Routes để nó luôn hiển thị trên mọi trang */}
       <Header /> 
 
-      {/* Bao bọc phần nội dung ở giữa */}
       <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
