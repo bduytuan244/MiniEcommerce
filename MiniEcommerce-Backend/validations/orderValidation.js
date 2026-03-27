6b12219c2dbc7f18c8ba6c68f9350cd18cda7711
bduytuan244 <bdtuan244@gmail.com>
2026-03-27 09:47:06 +0700

    Update logic phone

--- MiniEcommerce-Backend/validations/orderValidation.js
+++ MiniEcommerce-Backend/validations/orderValidation.js
@@ -5,17 +5,24 @@ const createOrderSchema = Joi.object({
         'array.min': 'Đơn hàng phải có ít nhất 1 sản phẩm', 
         'any.required': 'Thiếu danh sách sản phẩm' 
     }),
-    address: Joi.string().required().messages({ 
-        'any.required': 'Vui lòng cung cấp địa chỉ giao hàng', 
-        'string.empty': 'Địa chỉ không được để trống' 
-    }),
-    phone: Joi.string().required().messages({ 
-        'any.required': 'Vui lòng cung cấp số điện thoại', 
-        'string.empty': 'Số điện thoại không được để trống' 
-    }),
-    customerName: Joi.string().required().messages({ 
-        'any.required': 'Vui lòng cung cấp tên người nhận', 
-        'string.empty': 'Tên người nhận không được để trống' 
+    shippingInfo: Joi.object({
+        address: Joi.string().required().messages({ 
+            'any.required': 'Vui lòng cung cấp địa chỉ giao hàng', 
+            'string.empty': 'Địa chỉ không được để trống' 
+        }),
+        
+        phone: Joi.string().pattern(/^(0[3|5|7|8|9])+([0-9]{8})$/).required().messages({ 
+            'any.required': 'Vui lòng cung cấp số điện thoại', 
+            'string.empty': 'Số điện thoại không được để trống',
+            'string.pattern.base': 'Số điện thoại không hợp lệ (Phải là 10 số và bắt đầu bằng 03, 05, 07, 08, 09)'
+        }),
+        
+        fullName: Joi.string().required().messages({ 
+            'any.required': 'Vui lòng cung cấp tên người nhận', 
+            'string.empty': 'Tên người nhận không được để trống' 
+        })
+    }).required().messages({
+        'any.required': 'Thiếu thông tin giao hàng'
     }),
     paymentMethod: Joi.string().required().messages({ 
         'any.required': 'Vui lòng chọn phương thức thanh toán' 
